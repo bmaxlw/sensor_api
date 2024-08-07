c870c6fed466cecfbbcf1c9836cd51b543083c5e
dotpy <b.max.lw@gmail.com>
2024-08-07 13:25:02 +0200

    init

---
+++ src/database.py
@@ -0,0 +1,22 @@
+import redis, os
+
+def get_redis(redis_host: str = os.getenv('RED_HOST', 'localhost'), 
+              redis_port: str = os.getenv('RED_PORT', 6379)) -> redis.Redis:
+    return redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+def populate_redis(r_session: redis.Redis) -> None:
+    sensors_base = {
+        "d7828b2e-de80-4812-b0e9-a2f20ac41b71": {"min": 50.00, "max": 1900.00},
+        "d53e992d-584e-4cb2-829b-221d92cf1d35": {"min": 60.00, "max": 2000.00},
+        "abdcc6e2-3f7b-4f31-aaea-3dbede05c7ae": {"min": 70.00, "max": 2000.00},
+        "891aafc8-43c9-445b-9a54-4cedb94a676f": {"min": 50.00, "max": 1900.00},
+        "73ab7318-8e89-45d4-a570-dea7328abbd5": {"min": 70.00, "max": 2000.00},
+        "232d39f5-e254-4096-8fd5-09c1a5f55792": {"min": 100.00, "max": 2100.00},
+        "bfa43876-29c2-47ac-9b0e-6a840526e849": {"min": 100.00, "max": 2100.00},
+        "d26ce4ea-eed8-4d91-84cf-a4092e2ffcb1": {"min": 50.00, "max": 1900.00},
+        "14f1e9e9-2428-4bf4-8738-ebe89e0b4712": {"min": 75.00, "max": 150.00},
+        "8aa2940d-9c5e-4875-b32b-680f278acd8d": {"min": 100.00, "max": 200.00},
+    }
+
+    for k,v in sensors_base.items():
+        r_session.set(k,f"{v.get("min")},{v.get("max")}")
---
+++ src/main.py
@@ -0,0 +1,41 @@
+import datetime, random, src.database as db
+from fastapi import FastAPI
+
+app = FastAPI()
+red = db.get_redis()
+db.populate_redis(r_session=red)
+
+@app.get("/sensors")
+def get_temperature_sensors():
+
+    sensors: dict[str, dict] = {}
+    keys: list[str] = red.keys('*')
+    for key in keys:
+        sensors[key] = {"min": red.get(key).split(',')[0], 
+                        "max": red.get(key).split(',')[1]}
+
+    responses: list[dict] = [
+        {
+            "sensor_id": sid,
+            "sensor_date": datetime.date.today(),
+            "sensor_time": datetime.datetime.now().strftime("%H:%M:%S"),
+            "sensor_limit_low": tmap.get("min"), 
+            "sensor_limit_high": tmap.get("max"),
+            "sensor_temp_low": round(random.uniform(float(red.get(sid).split(",")[0]),
+                                              float(red.get(sid).split(",")[0])*1.05 if random.randint(1,10)%2==0 
+                                              else float(red.get(sid).split(",")[0])*0.95),2),
+            "sensor_temp_high": round(random.uniform(float(red.get(sid).split(",")[1]),
+                                              float(red.get(sid).split(",")[1])*1.05 if random.randint(1,10)%2==0 
+                                              else float(red.get(sid).split(",")[1])*0.95),2),
+            "sensor_temp_low_prev": float(red.get(sid).split(",")[0]),
+            "sensor_temp_high_prev": float(red.get(sid).split(",")[1])
+        } for sid, tmap in sensors.items()     
+    ]
+
+    redis_log = list(zip([sensor_id for sensor_id in sensors.keys()], 
+                         [f"{response.get("sensor_temp_low")},{response.get("sensor_temp_high")}" for response in responses]))
+    
+    for record in redis_log:
+        red.set(*record)
+
+    return responses
